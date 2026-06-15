61709ecb5be7ca5d314c1791afdc65bc669b317d
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-15 19:43:39 +0300

    feat: add form submission handling to Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { Form } from 'antd';
 import { Link } from 'react-router';
 const Login = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
   return (
     <div className="h-screen d-flex justify-content-center align-center align-items-center bg-primary">
       <div className="bg-white p-4 w-400">
-        <h3>SHEYJOBS - LOGIN</h3>
+        <h4>SHEYJOBS - LOGIN</h4>
         <div className="divider"></div>
-        <Form layout="vertical">
+        <Form layout="vertical" onFinish={onFinish}>
           <Form.Item name="email" label="Email">
             <input type="email" />
           </Form.Item>
           <Form.Item name="password" label="Password">
             <input type="password" />
           </Form.Item>
-          <button className="primary-contained-btn w-100 mt-2">Login</button>
+          <button type="submit" className="primary-contained-btn w-100 mt-2">
+            Login
+          </button>
           <Link to="/register" className="mt-2 d-block">
             Not a member? Click here to register
           </Link>
