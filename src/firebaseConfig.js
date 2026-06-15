cdd1635489fce176aa6ead78683ceb1753a8d59a
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-15 13:48:22 +0300

    chore: initialize project with Create React App and add basic configuration files

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAo3j4NZP9eSc1e4cX2MR463yjAjnSW3WY',
+  authDomain: 'chat-yt-df5cf.firebaseapp.com',
+  projectId: 'chat-yt-df5cf',
+  storageBucket: 'chat-yt-df5cf.firebasestorage.app',
+  messagingSenderId: '271376162290',
+  appId: '1:271376162290:web:f4a03aaff37f45a231df6b',
+};
+
+const app = initializeApp(firebaseConfig);
+const fireDB = getFirestore();
