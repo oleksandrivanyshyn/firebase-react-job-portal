277b9b1fc6d8cf48d5c567f665ad6c9df12a75e0
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-16 19:05:06 +0300

    feat: integrate navigation and loading states into Register page with Redux actions and hooks

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Form, message } from 'antd';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import { RegisterUser } from '../apis/authentication';
+import { useDispatch } from 'react-redux';
+import { HideLoading, ShowLoading } from '../redux/alertSlice';
 
 const Register = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const onFinish = async (values) => {
     try {
+      dispatch(ShowLoading());
       const response = await RegisterUser(values);
+      dispatch(HideLoading());
       if (response.success) {
         message.success(response.message);
+        navigate('/login');
       } else {
         message.error(response.message);
       }
     } catch (error) {
+      dispatch(HideLoading());
       message.error(error.message);
     }
   };
+
   return (
     <div className="h-screen d-flex justify-content-center align-center align-items-center bg-primary">
       <div className="bg-white p-4 w-400">
