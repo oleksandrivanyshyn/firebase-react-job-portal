ab94230f0b43868c61fc8520ac56c209d2e05b1d
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-16 11:04:01 +0300

    feat: implement PostedJobs and NewEditJob components with form fields and navigation

--- src/pages/user/postedjobs/NewEditJob.js
+++ src/pages/user/postedjobs/NewEditJob.js
@@ -1,7 +1,140 @@
 import React from 'react';
+import { useNavigate, useParams } from 'react-router';
+import PageTitle from '../../../components/PageTitle';
+import { Col, Form, Row } from 'antd';
 
 const NewEditJob = () => {
-  return <div>New Edit Job</div>;
+  const params = useParams();
+  const navigate = useNavigate();
+  return (
+    <div>
+      <PageTitle title={params.id ? 'Edit Job' : 'Add New Job Post'} />
+      <Form layout="vertical">
+        <Row gutter={[10, 10]}>
+          <Col span={12}>
+            <Form.Item
+              label="Title"
+              name="title"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <input type="text" />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              label="Industry"
+              name="industry"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <select name="" id="">
+                <option value="">Select</option>
+                <option value="it">IT</option>
+                <option value="finance">Finance</option>
+                <option value="marketing">Marketing</option>
+                <option value="realestate">Real Estate</option>
+              </select>
+            </Form.Item>
+          </Col>
+
+          <Col span={6}>
+            <Form.Item
+              label="Location"
+              name="location"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <select name="" id="">
+                <option value="">Select</option>
+                <option value="india">India</option>
+                <option value="usa">USA</option>
+                <option value="uk">UK</option>
+              </select>
+            </Form.Item>
+          </Col>
+
+          <Col span={6}>
+            <Form.Item
+              label="Company Name"
+              name="company"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <input type="text" />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              label="Salary"
+              name="salary"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <input type="text" />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              label="Job Type"
+              name="jobType"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <select name="" id="">
+                <option value="">Select</option>
+                <option value="fulltime">Full Time</option>
+                <option value="parttime">Part Time</option>
+                <option value="contract">Contract</option>
+              </select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              label="Last Date To Apply"
+              name="lastDateToApply"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <input type="date" />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              label="Experience"
+              name="experience"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <input type="text" />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              label="Notice Period"
+              name="noticePeriod"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <input type="text" />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              label="Job Description"
+              name="jobDescription"
+              rules={[{ required: true, message: 'required' }]}
+            >
+              <textarea type="text" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <div className="d-flex justify-content-end gap-2">
+          <button
+            className="primary-outlined-btn"
+            onClick={() => navigate('/posted-jobs')}
+          >
+            Cancel
+          </button>
+          <button className="primary-contained-btn" type="submit">
+            Save
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default NewEditJob;
--- src/pages/user/postedjobs/index.js
+++ src/pages/user/postedjobs/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PageTitle from '../../../components/PageTitle';
+import { useNavigate } from 'react-router';
 
 const PostedJobs = () => {
-  return <div>Posted Jobs</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div className="d-flex justify-content-between"></div>
+      <PageTitle title="Posted Jobs" />
+      <button
+        className="primary-outlined-btn"
+        onClick={() => navigate('/posted-jobs/new')}
+      >
+        New Job
+      </button>
+    </div>
+  );
 };
 
 export default PostedJobs;
